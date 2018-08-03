279e940dd3ccd5b4f49fdd758fa012706a87b44a
Taras Chuiko <taraschuiko@gmail.com>
2018-08-03 09:57:59 +0300

    fixed bugs in card form

--- src/js/script.js
+++ src/js/script.js
@@ -7,24 +7,29 @@ jQuery(document).ready(function($) {
 	});
 
 	$('input').on('focus', function(event) {
-		$(this).parent('.form__item').children('p').css('color', '#353535');
+		$(this).parent('.form__item, .card__item').children('p').css('color', '#353535');
 	});
 
 	$('input').on('blur', function(event) {
-		$(this).parent('.form__item').children('p').css('color', '#A7A7A7');
+		$(this).parent('.form__item, .card__item').children('p').css('color', '#A7A7A7');
 	});
 
 	$('.form__logo input[type=file]').change(function(event) {
 		$(this).parent('.form__logo').text('Change logo');
 	});
 
-	$('.card-number-input').on('change', function(event) {
-		if (this.value.length == this.maxLength) {
-			var $next = $(this).next('.card-number-input');
-			if ($next.length)
-				$(this).next('.card-number-input').focus();
-			else
-				$(this).blur();
+	$('.card-number-input').on('input', function(event) {
+		if ($(this).val().length == 4) {
+			console.log('4');
+			$(this).next('.card-number-input').focus();
 		}
+	});
+
+	$('div[class^=shipping-payment__method]').on('click', function(event) {
+		event.preventDefault();
+		$('div[class^=shipping-payment__method]').each(function(index, el) {
+			$(this).css('background', '#ffffff');
+		});
+		$(this).css('background', '#efefef');
 	});
 });
